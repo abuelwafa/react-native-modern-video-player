59e9b0e45e9eb187e94c3d2032083572beb89930
Mohamed Ali Abuelwafa <mohamed.abuelwafa@gmail.com>
2021-02-20 17:47:10 +0200

    chore: upgrade bob, commitlint, husky, and other dependencies

--- prettier.config.js
+++ prettier.config.js
@@ -2,8 +2,17 @@ module.exports = {
     semi: true,
     tabWidth: 4,
     printWidth: 100,
+    useTabs: false,
     bracketSpacing: true,
     jsxBracketSameLine: true,
     singleQuote: true,
     trailingComma: 'all',
+    overrides: [
+        {
+            files: '*.json',
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
 };
